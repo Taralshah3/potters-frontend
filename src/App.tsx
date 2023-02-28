590caa29a2b179fb84878d1849357a61bb4f75c1
Taral Shah <taralshah3@gmail.com>
2023-02-27 21:55:27 -0600

    first commit ayo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ClientRequest } from 'http';
 
 function App() {
+
+  // function handleFileChange(event) {
+  //   console.log("test");
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{marginLeft: "50%", marginTop: "5%"}}>
+      <h3> File Summarizer ting </h3>
+      <input type="file" onChange={handleFileChange} />
     </div>
   );
 }
